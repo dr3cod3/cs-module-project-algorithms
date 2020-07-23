01db2eee533b2954d5b968a6e8a405b1aed5b21a
Adebola Adesina <dr3cod3form3@outlook.com>
2020-07-23 12:31:18 -0700

    1 exercise done

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python3
 '''
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+
+def single_number(arr):
+    no_duplicate = set()
+    for elem in arr:
+        if arr[elem] in no_duplicate:
+            no_duplicate.remove(arr[elem])
+        else:
+            no_duplicate.add(arr[elem])
+            only_num = list(no_duplicate)[0]
+    return only_num
+    # Your code here
 
 
 if __name__ == '__main__':
