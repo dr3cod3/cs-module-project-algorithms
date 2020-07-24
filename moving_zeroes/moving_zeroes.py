faceee52ea6c295a5a2d2679fad607370b53bab3
Adebola Adesina <dr3cod3form3@outlook.com>
2020-07-24 11:40:35 -0700

    6 passed

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,31 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    '''
+    Solution that uses two pointers that start on either end of the array, counting 0s as the pointers move 
+    If the left pointer sees a 0 and the right pointer sees a non-zero, we swap their values
+    Otherwise, we increment the left pointer if it sees a non-zero value
+    We decrement the right pointer if it sees a 0
+    O(n) time, O(1) space
+    '''
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        if arr[left] == 0 and arr[right] != 0:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+        else:
+            if arr[left] != 0:
+                left += 1
+            if arr[right] == 0:
+                right -= 1
+
+    return arr
 
 
 if __name__ == '__main__':
